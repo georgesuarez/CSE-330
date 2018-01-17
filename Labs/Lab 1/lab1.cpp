361aecb224a1eaa4db5ff2453e1119ff0882906f
George Suarez <georgejsuarez@gmail.com>
2018-01-17 12:53:33 -0800

    Lab 1

---
+++ Labs/Lab 1/lab1.cpp
@@ -0,0 +1,72 @@
+#include <unistd.h>
+#include <iostream>
+
+class Time {
+ public:
+  Time(int h, int m);
+  int gethours() const;
+  int getminutes() const;
+  void addminute();
+  void minusminute();
+  bool operator==(Time t);
+  void operator++();     // prefix
+  void operator++(int);  // postfix
+  void operator--();     // prefix
+  void operator--(int);  // postfix
+ private:
+  int hours;
+  int minutes;
+};
+
+Time::Time(int hours, int minutes) : hours(hours), minutes(minutes) {}
+
+int Time::gethours() const { return hours; }
+
+int Time::getminutes() const { return minutes; }
+
+bool Time::operator==(Time t) {
+  if (this->gethours() != t.gethours()) return false;
+  if (this->getminutes() != t.getminutes()) return false;
+  return true;
+}
+
+void Time::operator++() { this->addminute(); }
+
+void Time::operator++(int) { this->addminute(); }
+
+void Time::operator--() { this->minusminute(); }
+
+void Time::operator--(int) { this->minusminute(); }
+
+void Time::addminute() {
+  minutes++;
+  if (minutes == 60) {
+    minutes = 0;
+    hours++;
+  }
+  if (hours == 13) hours = 1;
+}
+
+void Time::minusminute() {
+  minutes--;
+  if (minutes == -1) {
+    minutes = 59;
+    hours--;
+  }
+
+  if (hours == 0) hours = 12;
+}
+
+int main() {
+  Time start = Time(12, 0);
+  Time end = Time(1, 15);
+
+  for (int i = 0; i < 75; i++) {
+    sleep(60); // sleep for 60 seconds
+    end--;  
+  }
+
+  if (start == end) {
+    std::cout << "Class dismissed." << '\n';
+  }
+}
