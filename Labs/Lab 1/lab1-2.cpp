c2e15eb10c6b708f1e5d023f3e6c444220a3a24e
George Suarez <georgejsuarez@gmail.com>
2018-01-19 13:47:05 -0800

    Still need to finish infix_to_postfix function

--- Labs/Lab 1/lab1-2.cpp
+++ Labs/Lab 1/lab1-2.cpp
@@ -2,91 +2,99 @@
 #include <string>
 #include <vector>
 
-template<typename T>
+template <typename T>
 class Stack {
-public:
-    Stack();
-    void push(T data);
-    void pop();
-    T get_top();
-    bool is_empty();
-    int size() const;
-    T get(int location) const;
-private: 
-    std::vector<T> stack;
+ public:
+  Stack();
+  void push(T data);
+  void pop();
+  T get_top();
+  bool is_empty();
+  int size() const;
+  T get(int location) const;
+
+ private:
+  std::vector<T> stack;
 };
 
-template<typename T>
+template <typename T>
 Stack<T>::Stack() {}
 
-template<typename T>
+template <typename T>
 void Stack<T>::push(T data) {
-    stack.push_back(data);
+  stack.push_back(data);
 }
 
-template<typename T>
+template <typename T>
 void Stack<T>::pop() {
-    stack.pop_back();
+  stack.pop_back();
 }
 
-template<typename T>
+template <typename T>
 T Stack<T>::get_top() {
-    return stack.back();
+  return stack.back();
 }
 
-template<typename T>
+template <typename T>
 bool Stack<T>::is_empty() {
-    if (stack.size() == 0) {
-        return true;
-    }
-    else {
-        return false;
-    }
+  if (stack.size() == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-template<typename T>
+template <typename T>
 int Stack<T>::size() const {
-    return stack.size();
+  return stack.size();
 }
 
-template<typename T>
+template <typename T>
 T Stack<T>::get(int location) const {
-    return stack[location];
+  return stack[location];
 }
 
-
+bool is_operator(char x) {
+   if (x == '*' || x == '/' || x == '+' || x == '-') {
+       return true;
+   } 
+   else {
+       return false;
+   }
+}
 
 // TODO: Finish this function
 std::string infix_to_postfix(std::string x) {
-    Stack<char> s = Stack<char>();
-    s.push('[');
-    std::string y;
-    int index = 0; // index to y
+  Stack<char> s = Stack<char>();
+  s.push('[');
+  x.append("]");
+  std::string y;
+  int index = 0;  // index to y
 
-    while (s.get(index) != ']') {
-        if (s.get(index) == '(') {
-            if (s.get(index) == '*' || s.get(index) == '/') {
-                while (s.get_top() == '*') {
-                    // do something...
-                }
-            }
-        }
+  while (!s.is_empty()) {
+    for (int i = 0; i < x.length(); i++) {
+      s.push(x[i]);
+      if (is_operator(x[i])) {
+        y[i] = s.get_top();
+        s.pop();
+      }
     }
-    return y; 
-}
+  }
 
 int main() {
+  Stack<int> s = Stack<int>();
+  std::string test = "(A+B)*C";
+  test.append("]");
+  s.push(10);
+  s.push(20);
+  s.push(30);
+  s.push(40);
 
-    Stack<int> s = Stack<int>();
+  for (int i = 0; i < s.size(); i++) {
+    std::cout << s.get(i) << '\n';
+  }
 
-    s.push(10);
-    s.push(20);
-    s.push(30);
-    s.push(40);
-
-    for (int i = 0; i < s.size(); i++) {
-        std::cout << s.get(i) << '\n';
-    }
-    
-    return 0;
+  std::cout << "Top of the stack is: " << s.get_top() << '\n';
+  std::cout << test << '\n';
+  return 0;
 }
