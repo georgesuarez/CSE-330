d9a1dcca5d55a4236d000a9e66b0a08e762df89d
George Suarez <georgejsuarez@gmail.com>
2018-01-20 15:56:29 -0800

    infix_to_postfix function finshed

--- Labs/Lab 1/lab1-2.cpp
+++ Labs/Lab 1/lab1-2.cpp
@@ -3,98 +3,175 @@
 #include <vector>
 
 template <typename T>
-class Stack {
- public:
-  Stack();
-  void push(T data);
-  void pop();
-  T get_top();
-  bool is_empty();
-  int size() const;
-  T get(int location) const;
+class Stack
+{
+  public:
+    Stack();
+    void push(T data);
+    void pop();
+    T get_top();
+    bool is_empty();
 
- private:
-  std::vector<T> stack;
+  private:
+    std::vector<T> stack;
 };
 
 template <typename T>
 Stack<T>::Stack() {}
 
 template <typename T>
-void Stack<T>::push(T data) {
-  stack.push_back(data);
+void Stack<T>::push(T data)
+{
+    stack.push_back(data);
 }
 
 template <typename T>
-void Stack<T>::pop() {
-  stack.pop_back();
+void Stack<T>::pop()
+{
+    stack.pop_back();
 }
 
 template <typename T>
-T Stack<T>::get_top() {
-  return stack.back();
+T Stack<T>::get_top()
+{
+    return stack.back();
 }
 
 template <typename T>
-bool Stack<T>::is_empty() {
-  if (stack.size() == 0) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-template <typename T>
-int Stack<T>::size() const {
-  return stack.size();
-}
-
-template <typename T>
-T Stack<T>::get(int location) const {
-  return stack[location];
-}
-
-bool is_operator(char x) {
-   if (x == '*' || x == '/' || x == '+' || x == '-') {
-       return true;
-   } 
-   else {
-       return false;
-   }
-}
-
-// TODO: Finish this function
-std::string infix_to_postfix(std::string x) {
-  Stack<char> s = Stack<char>();
-  s.push('[');
-  x.append("]");
-  std::string y;
-  int index = 0;  // index to y
-
-  while (!s.is_empty()) {
-    for (int i = 0; i < x.length(); i++) {
-      s.push(x[i]);
-      if (is_operator(x[i])) {
-        y[i] = s.get_top();
-        s.pop();
-      }
+bool Stack<T>::is_empty()
+{
+    if (stack.size() == 0)
+    {
+        return true;
     }
-  }
+    else
+    {
+        return false;
+    }
+}
 
-int main() {
-  Stack<int> s = Stack<int>();
-  std::string test = "(A+B)*C";
-  test.append("]");
-  s.push(10);
-  s.push(20);
-  s.push(30);
-  s.push(40);
+bool is_operand(char x)
+{
+    if (x >= '0' && x <= '9')
+    {
+        return true;
+    }
+    if (x >= 'a' && x <= 'z')
+    {
+        return true;
+    }
+    if (x >= 'A' && x <= 'Z')
+    {
+        return true;
+    }
 
-  for (int i = 0; i < s.size(); i++) {
-    std::cout << s.get(i) << '\n';
-  }
+    return false;
+}
 
-  std::cout << "Top of the stack is: " << s.get_top() << '\n';
-  std::cout << test << '\n';
-  return 0;
+bool is_operator(char x)
+{
+    if (x == '*' || x == '/' || x == '+' || x == '-')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int prec(char x)
+{
+    int weight;
+    switch (x)
+    {
+        case '*':
+        case '/':
+            weight = 2;
+            break;
+        case '+':
+        case '-':
+            weight = 1;
+            break;
+        default:
+            weight = 0;
+            break;
+    }
+
+    return weight;
+}
+
+std::string infix_to_postfix(std::string x)
+{
+    Stack<char> s = Stack<char>();
+    std::string y;
+    int index = 0; 
+
+    while (x.length() > index)
+    {
+        if (is_operand(x[index]))
+        {
+            y += x[index];
+        }
+        else
+        {
+            if (x[index] == '(')
+            {
+                s.push(x[index]);
+            }
+            else if (x[index] == ')')
+            {
+                while (!s.is_empty() && s.get_top() != '(')
+                {
+                    y += s.get_top();
+                    s.pop();
+                }
+
+                if (!s.is_empty())
+                {
+                    s.pop();
+                }
+                else
+                {
+                    return "Error: No matching '('\n";
+                }
+            }
+            else if (is_operator(x[index]))
+            {
+                if (s.is_empty() || prec(s.get_top()) < prec(x[index]))
+                {
+                    s.push(x[index]);
+                }
+                else
+                {
+                    while (!s.is_empty() && prec(s.get_top()) >= prec(x[index]))
+                    {
+                        y += s.get_top();
+                        s.pop();
+                    }
+                    s.push(x[index]);
+                }
+            }
+            else
+            {
+                return "Error: Invalid input.\n";
+            }
+        }
+
+        index++;
+    }
+    while (!s.is_empty())
+    {
+        y += s.get_top();
+        s.pop();
+    }
+    return y;
+}
+
+int main()
+{
+    std::string infix_expr1 = "(A+B)*C-(D/(J+D))";
+
+    std::cout << infix_expr1 << " translates to: " << infix_to_postfix(infix_expr1) << '\n';
+    return 0;
 }
