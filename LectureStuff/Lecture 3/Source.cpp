702336e2e14e7283885d3659d6e485ebd77646ed
George Suarez <georgejsuarez@gmail.com>
2018-01-31 21:05:56 -0800

    Renamed 'lecture_stuff' to 'LectureStuff'

---
+++ LectureStuff/Lecture 3/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "Stack.h"
+
+int main() {
+    Stack<double> s = Stack<double>();
+    
+    s.push(3.14);
+    s.pop();
+    
+    
+    return 0;
+}
