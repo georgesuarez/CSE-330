354542860e60ea559720f3e4edb5299fa5accdc2
006098556 <006098556@csusb.edu@jb358-8.cse.csusb.edu>
2018-03-12 14:05:37 -0700

    Created some tests for Mlist and Mtree

--- Labs/Lab 6/Main.cpp
+++ Labs/Lab 6/Main.cpp
@@ -13,12 +13,37 @@
 int main()
 {
     srand(time(nullptr));
+    Mlist<int> list = Mlist<int>();
+
+    
     Mtree<int> tree = Mtree<int>();
 
     for (int i = 0; i < 10; i++)
     {
         tree.add(rand() % 100 + 1);
+        list.add(rand() % 100 + 1);
     }
+
+     std::cout << "======Mlist Testing======" << std::endl;
+
+    list.print();
+
+    int random_index = list.size() % 9;
+
+    std::cout << "Back of the list: " << list.getback() << std::endl;
+    int random_element = list[random_index];
+
+    list.del(random_element);
+    std::cout << "Deleting element " << random_element << std::endl;
+
+    random_index = list.size() % 9;
+    list.erase(random_index);
+
+    std::cout << "Erasing position " << random_index << std::endl;
+
+    list.print();
+
+    std::cout << "======Mtree Testing======" << std::endl;
 
     tree.add(0);
 
